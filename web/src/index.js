fd07b336697ff7e880cf8e6b8932121fa984e698
Carlos Cabezas <carloscabezascontra@gmail.com>
2018-06-14 14:00:10 +0200

    feat: add react-router and 404 page not found

--- web/src/index.js
+++ web/src/index.js
@@ -1,8 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {
+	BrowserRouter as Router,
+	Route,
+	Switch,
+	Redirect
+} from 'react-router-dom';
+import registerServiceWorker from './registerServiceWorker';
 import './index.css';
 import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import NotFound from './pages/NotFound';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const app = (
+	<Router>
+		<div>
+			<Switch>
+				<Route exact path="/" component={App} />
+				<Route component={NotFound} />
+			</Switch>
+		</div>
+	</Router>
+);
+
+ReactDOM.render(app, document.getElementById('root'));
 registerServiceWorker();
---
+++ web/src/pages/NotFound.js
@@ -0,0 +1,21 @@
+import React, {Component} from 'react';
+import image from '../assets/img/404.png';
+
+class NotFound extends Component {
+	render() {
+		const container = {
+			display: 'flex',
+			alignItems: 'center',
+			justifyContent: 'center',
+			height: '100vh'
+		};
+
+		return (
+			<div style={container}>
+				<img src={image} alt="Page Not Found" />
+			</div>
+		);
+	}
+}
+
+export default NotFound;
