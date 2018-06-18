6ec994f51f758635970a1d03944f82fad61701d5
Carlos Cabezas <carloscabezascontra@gmail.com>
2018-06-18 14:20:34 +0200

    feat: add specs to phone-mocks

--- api/assets/data/phones-mock.js
+++ api/assets/data/phones-mock.js
@@ -3,8 +3,27 @@ const phones = [
 		id: '1',
 		name: 'Nokia 8 Sirocco',
 		baseImg: 'https://dl.dropboxusercontent.com/s/ho2snhbs2ci4bmr/nokia8-01.png?dl=0',
-		brand: '',
+		brand: 'Nokia',
 		specs: {
+			os: 'Android 8.1',
+			display: {
+				size: '5.3"',
+				resolution: '1440x2560 pixels'
+			},
+			chip: 'Snapdragon 835',
+			ram: '6GB',
+			camera: {
+				front: '13MP',
+				back: '13MP'
+			},
+			capacity: '128GB',
+			measures: {
+				dimensions: '151.5 x 73.7 x 7.9 mm',
+				weight: '160g'
+			},
+			battery: '3090mAh',
+			secureAuth: 'FACE_ID',
+			colors: ['000000']
 		},
 		images: [
 		]
@@ -13,8 +32,27 @@ const phones = [
 		id: '2',
 		name: 'Iphone X',
 		baseImg: 'https://dl.dropboxusercontent.com/s/1ci1mlbiwjbqsz9/iphoneX-01.png?dl=0',
-		brand: '',
+		brand: 'Apple',
 		specs: {
+			os: 'iOS 11',
+			display: {
+				size: '5.8"',
+				resolution: '1125x2436 pixels'
+			},
+			chip: 'A11 Bionic chip',
+			ram: '3GB',
+			camera: {
+				front: '7MP',
+				back: '12MP'
+			},
+			capacity: '64GB - 256GB',
+			measures: {
+				dimensions: '143.6 x 70.9 x 7.7 mm',
+				weight: '174g'
+			},
+			battery: '2716mAh',
+			secureAuth: 'FACE_ID',
+			colors: ['E2E3E4', '262529']
 		},
 		images: [
 		]
@@ -23,8 +61,27 @@ const phones = [
 		id: '3',
 		name: 'Huawei P20',
 		baseImg: 'https://dl.dropboxusercontent.com/s/03m3uvu10n3fqpg/huaweiP20-01.png?dl=0',
-		brand: '',
+		brand: 'Huawei',
 		specs: {
+			os: 'Android 8.1',
+			display: {
+				size: '6.1"',
+				resolution: '1080x2240 pixels'
+			},
+			chip: 'Hisilicon Kirin 970',
+			ram: '6GB',
+			camera: {
+				front: '24MP',
+				back: '40MP'
+			},
+			capacity: '128GB',
+			measures: {
+				dimensions: '155 x 73.9 x 7.8 mm',
+				weight: '180g'
+			},
+			battery: '4000mAh',
+			secureAuth: 'TOUCH_ID',
+			colors: ['E5D1B6', '000000', '295094', '57598C', 'FDD3CB']
 		},
 		images: [
 		]
@@ -33,8 +90,27 @@ const phones = [
 		id: '4',
 		name: 'Xperia XZ2',
 		baseImg: 'https://dl.dropboxusercontent.com/s/blhw4r5hhlinyse/xperiaXZ2.png?dl=0',
-		brand: '',
+		brand: 'Sony',
 		specs: {
+			os: 'Android 8.0',
+			display: {
+				size: '5.7"',
+				resolution: '1080x2160 pixels'
+			},
+			chip: 'Snapdragon 845',
+			ram: '6GB',
+			camera: {
+				front: '5MP',
+				back: '19MP'
+			},
+			capacity: '64GB',
+			measures: {
+				dimensions: '153 x 72 x 11.1 mm',
+				weight: '198g'
+			},
+			battery: '3180mAh',
+			secureAuth: 'TOUCH_ID',
+			colors: ['A2AAAD', '33353B', '174A5B', '948794']
 		},
 		images: [
 		]
@@ -43,8 +119,27 @@ const phones = [
 		id: '5',
 		name: 'Huawei Y7',
 		baseImg: 'https://dl.dropboxusercontent.com/s/xgwep1tw7v3le5d/huaweiY7-01.png?dl=0',
-		brand: '',
+		brand: 'Huawei',
 		specs: {
+			os: 'Android 7.0',
+			display: {
+				size: '5.5"',
+				resolution: '720x1280 pixels'
+			},
+			chip: 'Snapdragon 435',
+			ram: '2GB',
+			camera: {
+				front: '8MP',
+				back: '12MP'
+			},
+			capacity: '16GB',
+			measures: {
+				dimensions: '153.6 x 76.4 x 8.4 mm',
+				weight: '165g'
+			},
+			battery: '4000mAh',
+			secureAuth: 'TOUCH_ID',
+			colors: ['E9DDB7', 'CCCCCC', 'E8E8E8']
 		},
 		images: [
 		]
@@ -53,8 +148,27 @@ const phones = [
 		id: '6',
 		name: 'LG Q6',
 		baseImg: 'https://dl.dropboxusercontent.com/s/er96j5xvlv4p7wk/lgQ6-01.png?dl=0',
-		brand: '',
+		brand: 'LG',
 		specs: {
+			os: 'Android 7.1',
+			display: {
+				size: '5.5"',
+				resolution: '1080x2160 pixels'
+			},
+			chip: 'Snapdragon 435',
+			ram: '4GB',
+			camera: {
+				front: '5MP',
+				back: '13MP'
+			},
+			capacity: '64GB',
+			measures: {
+				dimensions: '142.5 x 69.3 x 8.1 mm',
+				weight: '149g'
+			},
+			battery: '3000mAh',
+			secureAuth: 'FACE_ID',
+			colors: ['000000', 'CCCCCC']
 		},
 		images: [
 		]
--- web/src/pages/phone/detail/phone-detail.js
+++ web/src/pages/phone/detail/phone-detail.js
@@ -3,7 +3,9 @@ import React, {Component} from 'react';
 class PhoneDetail extends Component {
 	render() {
 		return (
-			<div>Detail</div>
+			<div>
+
+			</div>
 		);
 	}
 }
